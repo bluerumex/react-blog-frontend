6abd4493806fb5e96b61c23fce44f92ff7d16bb3
bluerumex <bluerumex@naver.com>
2018-10-27 23:28:12 +0900

    router ã page 생성

--- src/store/modules/post.js
+++ src/store/modules/post.js
@@ -1,6 +1,6 @@
 import { createAction, handleActions}  from 'redux-actions';
 import { Map } from 'immutable';
-import { pender } from 'react-pender';
+import { pender } from 'redux-pender';
 
 const initialState = Map({})
 
