bd657f3c5a8063b4e8692d46c4e78b1674d0c733
bluerumex <bluerumex@naver.com>
2018-10-30 11:45:39 +0900

    Editor page 추가

--- src/components/editor/EditorTemplate/EditorTemplate.js
+++ src/components/editor/EditorTemplate/EditorTemplate.js
@@ -6,13 +6,63 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 class EditorTemplate extends Component {
-  render() {
-    return (
-      <div>
-        EditorTemplate
-      </div>
-    );
-  }
-}
+    state = {
+        leftPercentage: 0.5
+    };
+
+    _handleMouseMove = (e) => {
+        console.log('mouse down...')
+        this.setState({
+            leftPercentage: e.clientX / window.innerWidth
+        });
+    };
+
+    _handleMouseUp = (e) => {
+        document.body.removeEventListener('mousemove', this._handleMouseMove);
+        window.removeEventListener('mouseup', this._handleMouseUp);
+    }
+
+    _handleSeparatorMouseDown = (e) => {
+        document.body.addEventListener('mousemove', this._handleMouseMove);
+        window.addEventListener('mouseup', this._handleMouseUp);
+    }
+
+    render() {
+        const { header, editor, preview } = this.props;
+        const { leftPercentage } = this.state;
+        const { _handleSeparatorMouseDown } = this;
+
+        const leftStyle = {
+            flex: leftPercentage
+        };
+
+        const rightStyle = {
+            flex: 1 - leftPercentage
+        };
+
+        const separatorStyle = {
+            left: `${leftPercentage * 100}%`
+        }
+
+        return (
+            <div className={cx('editor-template')}>
+                {header}
+                <div className={cx('panes')}>
+                    <div className={cx('pane, editor')} style={leftStyle}>
+                    {editor}    
+                    </div>
+                    <div className={cx('pane, preview')} style={rightStyle}>
+                    {preview}
+                    </div>
+                    <div
+                        className={cx('separator')}
+                        style={separatorStyle}
+                        onMouseDown={_handleSeparatorMouseDown}
+                    />
+                </div>
+            </div>
+        );
+    };
+};
 
 export default EditorTemplate;
--- src/components/editor/PreviewPane/PreviewPane.js
+++ src/components/editor/PreviewPane/PreviewPane.js
@@ -5,10 +5,17 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const PreviewPane = () => (
-  <div>
-    PreviewPane
-  </div>
-);
+const PreviewPane = () => {
+    return (
+        <div className={cx('preview-pane')}>
+            <h1 className={cx('title')}>
+                제목
+            </h1>
+            <div>
+                내용
+            </div>
+        </div>
+    );
+};
 
 export default PreviewPane;
