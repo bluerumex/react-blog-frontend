7b33019c5097462ae1475d33fe129b1230279a76
unknown <bluerumex@naver.com>
2018-11-07 02:42:25 +0900

    LoginModal

---
+++ src/components/modal/LoginModal/LoginModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './LoginModal.scss';
+import classNames from 'classnames/bind';
+import ModalWrapper from 'components/modal/ModalWrapper';
+
+const cx = classNames.bind(styles);
+
+const LoginModal = ({
+    visible, password, error, onCancel, onLogin, onChange, onKeyPress
+}) => (
+    <Mod
+)
+
