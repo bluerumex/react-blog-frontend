7bf4f09d015266db90e7c13a02d798d2a6f14936
unknown <bluerumex@naver.com>
2018-11-07 03:05:01 +0900

    LoginModal Style

--- src/components/modal/LoginModal/LoginModal.js
+++ src/components/modal/LoginModal/LoginModal.js
@@ -8,6 +8,19 @@ const cx = classNames.bind(styles);
 const LoginModal = ({
     visible, password, error, onCancel, onLogin, onChange, onKeyPress
 }) => (
-    <Mod
-)
+    <ModalWrapper visible={visible}>
+        <div className={cx('form')}>
+            <div onClick={onCancel} className={cx('close')}>&times;</div>
+            <div className={cx('title')}>로그인</div>
+            <div className={cx('description')}>관리자 비밀번호를 입력하세요</div>
+            <input autoFocus type="password" placeholder="비밀번호 입력" 
+                value={password} 
+                onChange={onchange} 
+                onKeyPress={onkeypress} />
+            { error & <div className={cx('error')}>로그인 실패</div> }
+            <div className={cx('login')} onClick={onLogin}>로그인</div>
+        </div>
+    </ModalWrapper>
+);
 
+export default LoginModal;
